a9de5e00f2bf5678d4bea3d2e60795f817cd959f
Mark Cooper (O365) <markcoop@microsoft.com>
2019-10-29 10:56:11 -0700

    Add project files.

---
+++ BankApp2019/Bank.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BankApp2019
+{
+    static class Bank
+    {
+        private static List<Account> accounts = new List<Account>();
+        public static Account CreateAccount(string accountName, string emailAddress, TypeOfAccount accountType = TypeOfAccount.Checking, decimal initialDeposit = 0)
+        {
+            var account = new Account
+            {
+                AccountName = accountName,
+                EmailAddress = emailAddress,
+                AccountType = accountType
+            };
+
+            if (initialDeposit > 0)
+            {
+                account.Deposit(initialDeposit);
+            }
+
+            accounts.Add(account);
+            return account;
+        }
+
+        public static IEnumerable<Account> GetAllAccountsByEmailAddress(string emailAddress)
+        {
+            return accounts.Where(a => a.EmailAddress == emailAddress);
+        }
+        
+    }
+}
